b589ee10854e961757fbd3518ec68a353b7df941
chenfyuan <windows.zom@live.cn>
2024-02-20 23:05:49 +0800

    Initial commit

---
+++ src/main/java/io/github/chenfyuan/rocketmqv5springbootdemo/Rocketmqv5SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.chenfyuan.rocketmqv5springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rocketmqv5SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rocketmqv5SpringBootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/chenfyuan/rocketmqv5springbootdemo/Rocketmqv5SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.chenfyuan.rocketmqv5springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rocketmqv5SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
